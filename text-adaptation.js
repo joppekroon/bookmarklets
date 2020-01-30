3ed8e8b5153e6051145916c86c1c8b62f02d3e84
Joppe Kroon <J.Kroon@topdesk.com>
2020-01-30 13:58:44 +0100

    Added '!important' for robustness

--- text-adaptation.js
+++ text-adaptation.js
@@ -3,12 +3,12 @@
   var paragraphs = document.querySelectorAll('p');
 
   Array.prototype.forEach.call(all, function(el) {
-    el.style.lineHeight = '1.5';
-    el.style.letterSpacing = '0.12em';
-    el.style.wordSpacing = '0.16em';
+    el.style.setProperty('line-height', '1.5', 'important');
+    el.style.setProperty('letter-spacing', '0.12em', 'important');
+    el.style.setProperty('word-spacing', '0.16em', 'important');
   });
 
   Array.prototype.forEach.call(paragraphs, function(p) {
-    p.style.marginBottom = '2em';
+    p.style.setProperty('margin-bottom', '2em', 'important');
   });
 })();
