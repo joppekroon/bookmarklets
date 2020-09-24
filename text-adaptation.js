460d9b2a2566010c9a09d73ab5fa7576b22c8c6e
Joppe Kroon <J.Kroon@topdesk.com>
2020-09-24 10:02:51 +0200

    #1 Text adaptation now works on child frames as well

--- text-adaptation.js
+++ text-adaptation.js
@@ -1,14 +1,27 @@
 (function() {
-  const all = document.querySelectorAll('*');
-  const paragraphs = document.querySelectorAll('p');
+  adapt(window);
   
-  Array.prototype.forEach.call(all, function(el) {
-    el.style.setProperty('line-height', '1.5', 'important');
-    el.style.setProperty('letter-spacing', '0.12em', 'important');
-    el.style.setProperty('word-spacing', '0.16em', 'important');
-  });
-
-  Array.prototype.forEach.call(paragraphs, function(p) {
-    p.style.setProperty('margin-bottom', '2em', 'important');
-  });
+  function adapt(win) {
+    const all = win.document.querySelectorAll('*');
+    const paragraphs = win.document.querySelectorAll('p');
+    
+    Array.prototype.forEach.call(all, function(el) {
+      el.style.setProperty('line-height', '1.5', 'important');
+      el.style.setProperty('letter-spacing', '0.12em', 'important');
+      el.style.setProperty('word-spacing', '0.16em', 'important');
+    });
+  
+    Array.prototype.forEach.call(paragraphs, function(p) {
+      p.style.setProperty('margin-bottom', '2em', 'important');
+    });
+    
+    const frames = win.frames;
+    for (let i = 0; i < frames.length; i++) {
+      try {
+        adapt(frames[i].window);
+      } catch (err) {
+        console.log(frames[i].location, err);
+      }
+    }
+  }
 })();
